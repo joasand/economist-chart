ba3dd4099436811d18271cc5dfb55a4c3b735b85
joasand <jr.san.91@hotmail.com>
2026-04-05 23:51:01 +0200

    final draft

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ export const data = [
 
 export default function App() {
 
-  const width = 800;
-  const height = 500;
+  const width = 650;
+  const height = 360;
 
   const xScale = scaleLinear()
-    .domain([0, 70])
+    .domain([0, 57])
     .range([0, width]);
 
   const yScale = scaleBand()
@@ -28,7 +28,40 @@ export default function App() {
     .range([0, height])
     .padding(0.3);
 
+  const gridLines = Array.from({ length: 15 }, (_, i) => i * 5).map((tick) => (
+    <>
+    <line 
+      x1={0}
+      x2={0}
+      y1={0+10}
+      y2={height-10}
+      stroke="black"
+    />
+    <line
+      key={`grid-${tick}`}
+      x1={xScale(tick)}
+      x2={xScale(tick)}
+      y1={0+10}
+      y2={height-10}
+      stroke="#d3d3d3"
+      strokeWidth="1"
+      opacity={0.7}
+    />
+     <text
+      key={`grid-${tick}`}
+      x={xScale(tick)}
+      y={-3}
+      textAnchor={tick === 0 ? "start" : "middle"}
+      fill="rgb(128, 128, 128)"
+      fontSize="12"
+      >
+      {tick}
+    </text>
+    </>
+  ));
+
   const allBars = data.sort((a, b) => b.count - a.count).map((d, i) => (
+    <>
     <g key={i}>
       <rect
         key={i}
@@ -36,26 +69,75 @@ export default function App() {
         y={yScale(d.name)}
         width={xScale(d.count)}
         height={yScale.bandwidth()}
-        fill="#69b3a2"
+        fill="#076fa2"
         />
     <text
       x={5}
       y={yScale(d.name) + yScale.bandwidth() / 2}
-      textAnchor="start"
+      textAnchor={d.count < 10 ? "middle" : "start"}
       dy="0.3em"
+      dx={d.count < 8 ? xScale(9.5) : d.count < 10 ? xScale(11) : 0}
       fontSize="14"
+      fill={d.count > 10 ? "white" : "#076fa2"}
       >
       {d.name}
     </text>
     </g>
+    </>
   ));
+
+  const decorativeLines = () => (
+     <>
+     <svg width={800} height={60}>
+      <line
+        x1={0}
+        x2={xScale(53)}
+        y1={50}
+        y2={50}
+        stroke="rgb(229, 1, 28)"
+        strokeWidth="2"
+      />  
+      <rect 
+      x={0} 
+      y={50} 
+      width={30} 
+      height={10} 
+      fill="rgb(229, 1, 28)" />
+    </svg>
+    </>
+  )
+
+  const descriptiveText = () => (
+      <>
+    <h1>Escape artists</h1>
+    <div style={{ textAlign: 'left',
+      fontSize: '15px',
+      color: 'black',
+      marginTop: '0px',
+      marginBottom: '14px',
+      fontFamily: 'var(--sans)',
+    }}>Number of laboratory-acquired infections, 1970-2021</div>
+
+      </>
+  )
+
+  const footerText = () => (
+      <>
+    <footer style={{ marginTop: '20px', fontSize: '12px', color: 'rgb(128, 128, 128)', textAlign: 'left' }}>
+      Sources: Laboratory-Acquired Infection Database; American Biological Safety Association
+    </footer>
+      </>
+  )
 
   return (
     <>
-    <h1>Economist Chart</h1>
-    <svg width={width} height={height}>
+    {decorativeLines()}
+    {descriptiveText()}
+    <svg width={width} height={height + 20} viewBox={`0 -20 ${width} ${height + 20}`}>
+      {gridLines}
       {allBars}
     </svg>
+    {footerText()}
     </>
   );
 }
