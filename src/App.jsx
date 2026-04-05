69f5cdf8dc0427fcb821e1a9d8f2a9051e710859
joasand <jr.san.91@hotmail.com>
2026-04-05 13:45:10 +0200

    first draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { scaleBand, scaleLinear } from 'd3'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const data = [
+  { count: 6, name: "Hantavirus" },
+  { count: 7, name: "Tularemia" },
+  { count: 7, name: "Dengue" },
+  { count: 9, name: "Ebola" },
+  { count: 11, name: "E. coli" },
+  { count: 15, name: "Tuberculosis" },
+  { count: 17, name: "Salmonella" },
+  { count: 18, name: "Vaccinia" },
+  { count: 54, name: "Brucella" },
+];
+
+export default function App() {
+
+  const width = 800;
+  const height = 500;
+
+  const xScale = scaleLinear()
+    .domain([0, 70])
+    .range([0, width]);
+
+  const yScale = scaleBand()
+    .domain(data.map(d => d.name))    
+    .range([0, height])
+    .padding(0.3);
+
+  const allBars = data.sort((a, b) => b.count - a.count).map((d, i) => (
+    <g key={i}>
+      <rect
+        key={i}
+        x={0}
+        y={yScale(d.name)}
+        width={xScale(d.count)}
+        height={yScale.bandwidth()}
+        fill="#69b3a2"
+        />
+    <text
+      x={5}
+      y={yScale(d.name) + yScale.bandwidth() / 2}
+      textAnchor="start"
+      dy="0.3em"
+      fontSize="14"
+      >
+      {d.name}
+    </text>
+    </g>
+  ));
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    <h1>Economist Chart</h1>
+    <svg width={width} height={height}>
+      {allBars}
+    </svg>
     </>
-  )
+  );
 }
-
-export default App
